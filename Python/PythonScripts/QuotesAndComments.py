16253132acd4b8a32418cbc1ad358249ea7bb0fd
Ghostdog02 <alex.vesely07@gmail.com>
2026-02-22 17:04:50 +0200

    InitialCommit

---
+++ Python/PythonScripts/QuotesAndComments.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+fasfassafas
+"""
+
+skill = "DevOps"
+print(skill)
+
+print("""
+This is multiple line
+This is multiple line
+This is multiple line
+""")
---
+++ Python/PythonScripts/Variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+var1 = "faskfa"
+var2 = 75
+var3 = 3.5
+
+print (var1)
+print (var2)
+print (var3)
